e539884638bebf79ef212b7531b0121e66d31b07
Praveen L <praveenl@guidanz.com>
2015-03-23 11:48:36 +0530

    all files

--- doppleML/src/com/doppleml/servicelayer/AuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.doppleml.servicelayer;
-
-public class AuthenticationService {
-	
-	//read data from json or table authenticate users
-	//get users role index and site from the user table/json
-	//get dashboards for the role and index from dashboards table 
-
-}
